16a26f4c79ffc935a9f7ed93064977acaf8ca0c3
KoutaYamazawa <kouta0810277@gmail.com>
2018-04-18 18:13:01 +0900

    Lesson5 課題
    途中作成。画像共有の許可の承諾を確認できる。

--- app/src/main/java/jp/techacademy/yamazawa/kouta/autoslideshowapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/yamazawa/kouta/autoslideshowapp/MainActivity.java
@@ -1,6 +1,7 @@
 package jp.techacademy.yamazawa.kouta.autoslideshowapp;
 
 import android.Manifest;
+import android.app.AlertDialog;
 import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.pm.PackageManager;
@@ -14,6 +15,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -24,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int PERMISSIONS_REQUEST_CODE = 100;
 
+    //メンバ変数
+    //インスタンス
     Button Return_button;
     Button AutoPlay_button;
     Button MoveOn_button;
@@ -43,62 +47,51 @@ public class MainActivity extends AppCompatActivity {
         Return_button = (Button) findViewById(R.id.button);
         AutoPlay_button = (Button) findViewById(R.id.button2);
         MoveOn_button = (Button) findViewById(R.id.button3);
-        Permission_button = (Button) findViewById(R.id.button5);
 
-        Permission_button.setOnClickListener(new View.OnClickListener() {
+        Log.d("ANDROID","始まります。");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) { //AndroidOSバージョン確認。6.0以降のバージョンか？
+            Log.d("Version","6.0以降");
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                //許可されている
+                // 画像の情報を取得する
+                getContentsInfo();
+            } else {
+                //拒否されている
+                Log.d("ANDROID", "許可されていない");
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_CODE);
+            }
+        }else{
+            // 画像の情報を取得する
+            getContentsInfo();
+            Log.d("Version","6.0以前");
+        }
+
+        MoveOn_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //パーミッションの許可状態を確認する
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) { //AndroidOSバージョン確認。6.0以降のバージョンか？
-                    if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                        //許可されている
-                        // 画像の情報を取得する
-                        getContentsInfo();
-                        Log.d("ANDROID", "許可されている");
-                    } else {
-                        //拒否されている
-                        Log.d("ANDROID", "許可されていない");
-                        requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_CODE);
-                    }
-                }
+                Log.d("ANDROID","進むボタンを押しました。");
+                getContentsInfo();
+                Log.d("ANDROID","画像を取得しました。");
+                //次の画像を表示
+                //cursor.moveToNext();
+                Log.d("ANDROID","次の画像です。");
             }
         });
 
-        /*AutoPlay_button.setOnClickListener(new View.OnClickListener() {
+        AutoPlay_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                for (; mTimerSec == 2.0; mTimerSec+=0.1) {
-                    //Timer 用のサブスレッド生成
-                    //タイマーの作成
-                    mTimer = new Timer();
 
-                    //タイマーの始動
-                    mTimer.schedule(new TimerTask() {
-
-                        //次の画像を2秒ごとに自動送りで表示
-                        @Override
-                        public void run() {
-                            mTimerSec += 0.1;
-
-                            mHandler.post(new Runnable() {
-                                @Override
-                                public void run() {
-                                    cursor.moveToNext();
-                                }
-                            });
-                        }
-                    }, 100, 100);
-                }
             }
-        });*/
-    }
+        });
+    };
 
     // 画像の情報を取得する
     public void getContentsInfo() {
 
         // 画像の情報を取得する
         ContentResolver resolver = getContentResolver();
-        Cursor cursor = resolver.query(
+        cursor = resolver.query(
                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
                 null, // 項目(null = 全項目)
                 null, // フィルタ条件(null = フィルタなし)
@@ -106,15 +99,16 @@ public class MainActivity extends AppCompatActivity {
                 null // ソート (null ソートなし)
         );
 
-        if (cursor.moveToFirst()) {
-            do {
-                // indexからIDを取得し、そのIDから画像のURIを取得する
-                int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-                Long id = cursor.getLong(fieldIndex);
-                Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+        if (cursor.moveToFirst()) { //検索結果が1つも無ければfalseを返すのでif文の{}には入らずすぐにcloseメソッドを呼び出します。
 
-                Log.d("ANDROID", "URI : " + imageUri.toString());
-            } while (cursor.moveToNext());  //次の画像は、ありますか？
+            //1枚の画像表示
+            int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+            Long id = cursor.getLong(fieldIndex);
+            Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+
+            ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
+            imageVIew.setImageURI(imageUri);
+
         }
         cursor.close();
     }
